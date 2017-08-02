070f3bb532a96063517e2e4574f3f80090a1c341
HelloBetafish <bethany.pfeister@gmail.com>
2017-08-02 15:02:52 -0700

    added css and bootstrap styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-
 // Define array of possibilities
 var alphabet = ["a", "b", "c", "d", "e", "f", "g",
 	"h", "i", "j", "k", "l", "m", "n", "o","p","q",
@@ -23,33 +22,33 @@ document.onkeyup = function(event){
 		if(guessesLeft > 0){
 
 			if (userInput.toLowerCase() === alphabet[randomIndex]){
-				alert("You are correct! The letter I was thinking was " + alphabet[randomIndex]);
+				alert("You are correct! The letter I was thinking was \'" + alphabet[randomIndex] + "\'.");
 				winTotal++;
-				document.getElementById("wins").innerHTML = "Wins: " + winTotal;
+				document.getElementById("wins").innerHTML = " Wins: " + winTotal;
 				guessesLeft = 10;
-				document.getElementById("gLeft").innerHTML = "Guesses Left: " + guessesLeft;
-				document.getElementById("guesses").innerHTML = "Your Guesses so far: "; 
+				document.getElementById("gLeft").innerHTML = " Guesses Left: " + guessesLeft;
+				document.getElementById("guesses").innerHTML = " Your Guesses so far: "; 
 				randomIndex = Math.floor(Math.random() * alphabet.length);
 				}	
 
 			else if (userInput.toLowerCase() != alphabet[randomIndex]){
 				guessesLeft--;
-				document.getElementById("gLeft").innerHTML = "Guesses Left: " + guessesLeft;
+				document.getElementById("gLeft").innerHTML = " Guesses Left: " + guessesLeft;
 				}
 		}
 
 		else{
-			alert("You ran out of guesses! The letter I was thinking of was " + alphabet[randomIndex]);
+			alert("You ran out of guesses! The letter I was thinking of was \'" + alphabet[randomIndex] + "\'.");
 			lossTotal++;
-			document.getElementById("losses").innerHTML = "Losses: " + lossTotal;
+			document.getElementById("losses").innerHTML = " Losses: " + lossTotal;
 			guessesLeft = 10;
-			document.getElementById("gLeft").innerHTML = "Guesses Left: " + guessesLeft;
-			document.getElementById("guesses").innerHTML = "Your Guesses so far: "; 
+			document.getElementById("gLeft").innerHTML = " Guesses Left: " + guessesLeft;
+			document.getElementById("guesses").innerHTML = " Your Guesses so far: "; 
 			randomIndex = Math.floor(Math.random() * alphabet.length);
 		}
 	}
 
 	else{
-		alert("Please choose a valid letter");
+		alert("Please choose a valid letter.");
 	}
 }
